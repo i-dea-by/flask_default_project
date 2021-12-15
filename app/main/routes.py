ee75f9f06a57490e262e20aa44a734b0c6abc5e8
egor daniloff <i@dea.by>
2021-12-15 18:31:53 +0300

    * routes.py * index.html - about.html
    + добавил
    - убрал/удалил
    * исправил/подправил

--- app/main/routes.py
+++ app/main/routes.py
@@ -10,9 +10,4 @@ module = Blueprint(
 
 @module.route('/')
 def index():
-    return render_template('index.html', title='индекс')
-
-
-@module.route('/about/')
-def about():
-    return render_template('about.html')
+    return render_template('index.html', title='default index page')
