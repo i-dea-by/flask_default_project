cc1be294a6de166b524d0f8b46e9ffc5fee3e462
egor daniloff <i@dea.by>
2021-12-18 11:54:32 +0300

    + models.py с таблицей пользователей для example
    + добавил
    - убрал/удалил
    * исправил/подправил

---
+++ app/example/models.py
@@ -0,0 +1,15 @@
+from app import db
+
+
+class User(db.Model):
+    """ example data model for user accounts """
+
+    __tablename__ = 'example_flask-default-project_users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return f'<User {self.username}>'
